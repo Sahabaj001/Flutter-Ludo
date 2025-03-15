dcfae6db69bb43f9cb7480aefd59d66960d16b1c
Sahabaj001 <147114734+Sahabaj001@users.noreply.github.com>
2025-03-15 17:12:21 +0530

    Update ludo_player.dart

--- lib/game_internals/ludo_player.dart
+++ lib/game_internals/ludo_player.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 import '/game_widgets/pawn_widget.dart';
 
-///This is ludo player model class which contains all the information about the player
 class LudoPlayer {
   ///Player type
   final LudoPlayerType type;
@@ -49,39 +48,32 @@ class LudoPlayer {
     }
   }
 
-  ///Get how many pawns are in the home
   int get pawnInsideCount => pawns.where((element) => element.step == -1).length;
 
-  ///Get how many pawns are outside home
   int get pawnOutsideCount => pawns.where((element) => element.step > -1).length;
 
-  ///Moving mean you'll replace the current widget with the new widget
   void movePawn(int index, int step) async {
     pawns[index] = PawnWidget(index, type, step: step, highlight: false);
   }
 
-  ///Highlight the pawn
   void highlightPawn(int index, [bool highlight = true]) {
     var pawn = pawns[index];
     pawns.removeAt(index);
     pawns.insert(index, PawnWidget(index, pawn.type, highlight: highlight, step: pawn.step));
   }
 
-  ///Highlight all the pawns
   void highlightAllPawns([bool highlight = true]) {
     for (var i = 0; i < pawns.length; i++) {
       highlightPawn(i, highlight);
     }
   }
 
-  ///Highlight pawn outside `HOME`
   void highlightOutside([bool highlight = true]) {
     for (var i = 0; i < pawns.length; i++) {
       if (pawns[i].step != -1) highlightPawn(i, highlight);
     }
   }
 
-  ///Highlight pawn inside `HOME`
   void highlightInside([bool highlight = true]) {
     for (var i = 0; i < pawns.length; i++) {
       if (pawns[i].step == -1) highlightPawn(i, highlight);
